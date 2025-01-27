7a30d07956d11c47e30c9afda7b7a8b77bb96c0e
Siyona-13 <immievashishth@gmail.com>
2025-01-27 21:29:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import MarkAttendancePage from "./pages/MarkAttendancePage"; // Adjust the path 
 function App() {
   return (
     <Router>
+      <div className="p-4">
+        <nav className="flex justify-between mb-4">
+          <Link to="/" className="text-blue-500">Mark Attendance</Link>
+          <Link to="/attendance-report" className="text-blue-500">View Attendance Report</Link>
+        </nav>
       <Routes>
         {/* Mark Attendance is now the default route */}
         <Route path="/" element={<MarkAttendancePage />} />
 
         {/* Add Face moved to /add-face */}
         <Route path="/add-face" element={<AddFacePage />} />
+        <Route path="/attendance-report" element={<AttendanceReport />} />
       </Routes>
+      </div>
     </Router>
   );
 }
